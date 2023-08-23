1f9fe2b77eb47e2b90ad70dcfc2327b807c566d9
santagomorelli <77756351+santagomorelli@users.noreply.github.com>
2023-08-22 23:07:28 -0300

    Update NumeroFlotanteMorelli.java
    comments added

--- NumeroFlotanteMorelli.java
+++ NumeroFlotanteMorelli.java
@@ -1,12 +1,14 @@
 
 public class NumeroFlotanteMorelli {
-    public static void main(String[] args) {
-       
+   public static void main(String[] args) {
+        //se delcara el numero decimal a trabajar
         float input = 2.13f;
-        
+        //se  convierte explicitamente el numero decimal a entero y se obtiene solo la parte entera (se guarda en una var tipo int)
         float parteEntera = (int) input;
+        //se  resta al numero decimal la parte entera y se obtiene la parte decimal (se guarda en una var tipo float)
         float parteDecimal = input - parteEntera;
 
+        //se imprimen los resultados en lineas separadas
         System.out.println("Parte entera: " + parteEntera);
         System.out.println("Parte entera: " + parteDecimal);
     
