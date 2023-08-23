5b6018375fe4dfebefdb375c01278800f23045b0
santagomorelli <77756351+santagomorelli@users.noreply.github.com>
2023-08-22 23:03:45 -0300

    Add files via upload

---
+++ NumeroFlotanteMorelli.java
@@ -0,0 +1,15 @@
+
+public class NumeroFlotanteMorelli {
+    public static void main(String[] args) {
+       
+        float input = 2.13f;
+        
+        float parteEntera = (int) input;
+        float parteDecimal = input - parteEntera;
+
+        System.out.println("Parte entera: " + parteEntera);
+        System.out.println("Parte entera: " + parteDecimal);
+    
+    }
+
+}
